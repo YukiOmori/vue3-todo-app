2a1e134d07d469c78826fcbba0fc0001fccb5040
Yuki Omori <yukiomori4211@gmail.com>
2021-07-30 19:55:40 +0900

    feat: Show todo list

---
+++ src/views/todos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="todo-list">
+    <div v-for="todo in todoStore.state.todos" :key="todo.id">
+      <p>{{ todo.title }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from 'vue'
+import { todoKey } from '@/store/todo'
+
+export default defineComponent({
+  setup () {
+    const todoStore = inject(todoKey)
+    if (!todoStore) {
+      throw new Error('todoStore is not provided')
+    }
+
+    return {
+      todoStore
+    }
+  }
+})
+</script>
